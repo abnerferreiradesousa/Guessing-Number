5108aeca83855aec11c1e17ab40a4714c0dfe958
Tuliolivieri <tuliolivieri@gmail.com>
2022-07-25 15:00:32 -0300

    altera os testes

--- src/guessing-number.Test/TestFirstReq.cs
+++ src/guessing-number.Test/TestFirstReq.cs
@@ -10,28 +10,28 @@ namespace guessing_number.Test;
 public class TestFirstReq
 {
     [Theory(DisplayName = "Deve exibir mensagem inicial!")]
-    [InlineData()]
+    [InlineData(new object[] {new string[]{"---Bem-vindo ao ...---", "Para começar..."}})]
     public void TestPrintInitialMessage(string[] expected)
     {
         throw new NotImplementedException();                                                   
     }
 
     [Theory(DisplayName = "Deve receber a entrada do usuário e converter para int")]
-    [InlineData()]
+    [InlineData("0", 0)]
     public void TestReceiveUserInputAndConvert(string entry, int expected)
     {        
         throw new NotImplementedException();
     }
 
     [Theory(DisplayName = "Deve receber a entrada do usuário e garantir que é um numérico")]
-    [InlineData()]
+    [InlineData(new object[] {new string[]{"10,", "10"}, 10})]
     public void TestReceiveUserInputAndVerifyType(string[] entrys, int expected)
     {
         throw new NotImplementedException();  
     }
 
     [Theory(DisplayName = "Deve receber a entrada do usuário e garantir que está entre -100 e 100!")]
-    [InlineData()]
+    [InlineData(new object[] {new string[]{"1000", "10"}, 10})]
     public void TestReceiveUserInputAndVerifyRange(string[] entrys, int expected)
     {
         throw new NotImplementedException();
--- src/guessing-number.Test/TestSecondReq.cs
+++ src/guessing-number.Test/TestSecondReq.cs
@@ -11,27 +11,27 @@ namespace guessing_number.Test;
 public class TestSecondReq
 {
     [Theory(DisplayName = "Deve escolher randomicamente um número entre -100 e 100!")]
-    [InlineData()]
+    [InlineData(-100, 100)]
     public void TestRandomlyChooseANumber(int MinimumRange, int MaximumRange)
     {
         throw new NotImplementedException();
     }
 
     [Theory(DisplayName = "Deve comparar a entrada do usuário em um caso MENOR")]
-    [InlineData()]
+    [InlineData(50, 0)]
     public void TestProgramComparisonValuesLess(int mockValue, int entry)
     {
         throw new NotImplementedException();   
     }
     [Theory(DisplayName = "Deve comparar a entrada do usuário em um caso MAIOR")]
-    [InlineData()]
+    [InlineData(50, 60)]
     public void TestProgramComparisonValuesBigger(int mockValue, int entry)
     {
         throw new NotImplementedException();  
     }
     
     [Theory(DisplayName = "Deve comparar a entrada do usuário em um caso MAIOR")]
-    [InlineData()]
+    [InlineData(50, 50)]
     public void TestProgramComparisonValuesEqual(int mockValue, int entry)
     {
         throw new NotImplementedException();
