92426185c57f1bed51d7f1f1ccb54fe71fb76313
Abner Ferreira de Sousa <abnerferreira84@gmail.com>
2022-08-03 09:53:30 -0300

    [feat]: console na vairavel error

--- src/guessing-number/GuessingGame.cs
+++ src/guessing-number/GuessingGame.cs
@@ -23,25 +23,39 @@ public class GuessNumber
         Console.Write(
             $"Para começar, tente adivinhar o número que eu pensei, entre -100 e 100! \n"
         );
+        // var res = Console.ReadLine();
+        // try {
+        //     int convert = Convert.ToInt32(res);
+        //     if(convert > 100 || convert < -100) 
+        //     {
+        //         Console.Write("Por favor, digite um número inteiro: \n");
+        //         Greet();
+        //     }
+        //     response = res;
+        // } catch(Exception error) {
+        //     Console.WriteLine("Por favor, digite um número inteiro: \n");
+        //     Greet();
+        // }    
+    }
+
+    public void ChooseNumber()
+    {
         var res = Console.ReadLine();
         try {
             int convert = Convert.ToInt32(res);
             if(convert > 100 || convert < -100) 
             {
                 Console.Write("Por favor, digite um número inteiro: \n");
-                Greet();
+                ChooseNumber();
             }
             response = res;
+            userValue = convert;
         } catch(Exception error) {
             Console.WriteLine("Por favor, digite um número inteiro: \n");
-            Greet();
-        }    
-    }
-
-    public void ChooseNumber()
-    {
-        int convert = Convert.ToInt32(response);
-        userValue = convert;
+            Console.WriteLine(error);
+            ChooseNumber();
+        }   
+        // int convert = Convert.ToInt32(response);
     }
     
     public void RandomNumber()
@@ -53,15 +67,18 @@ public class GuessNumber
     {
         if (userValue > randomValue)
         {
-            Console.Write("Tente um número MENOR");
-            Greet();
+            Console.Write("Tente um número MENOR \n");
+            // ChooseNumber();
         }
         if (userValue < randomValue) 
         {
-            Console.Write("Tente um número MAIOR");
-            Greet();
+            Console.Write("Tente um número MAIOR \n");
+            // ChooseNumber();
         }
-        if (userValue < randomValue)
+        if (userValue == randomValue) 
+        {
+            Console.WriteLine(userValue.ToString(), randomValue);
             Console.Write("ACERTOU!");
+        }
     }
 }
